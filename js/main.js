5dfb836c616a119b1d677589cb35b45acb0b2f72
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-05-22 17:20:23 +0200

    [Bonus 2] Click on text and invert done value

--- js/main.js
+++ js/main.js
@@ -13,7 +13,15 @@ createApp({
                 {
                     text: 'Appuntamento in banca alle 14:30',
                     done: true,
-                }
+                },
+                {
+                    text: 'Spararmi',
+                    done: false,
+                },
+                {
+                    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque praesentium amet quisquam fugit nemo eius ipsa voluptatum deserunt dolor eos. Quasi velit quo itaque sit nulla aliquid placeat explicabo officiis.',
+                    done: false,
+                },
             ],
         }
     },
